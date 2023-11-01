8c1ec1659cdf15f5cf6fe4bf73fdeceab367be7c
Mervin <mengqing723@gmail.com>
2023-11-01 13:52:25 +0800

    feat: 初始化基础配置&playground

---
+++ playground/vue3/vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      'vue': fileURLToPath(new URL('./node_modules/vue', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  optimizeDeps: {
+    exclude: ['vue-demi']
+  }
+})
